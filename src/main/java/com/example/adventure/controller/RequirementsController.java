f03dae4f35a41bdb9fa00663d41423bf20e59db4
Horrid Entity <etienne.olieu@gmail.com>
2022-09-26 10:41:17 +0200

    Controller for req placeholder

--- src/main/java/com/example/adventure/controller/RequirementsController.java
+++ src/main/java/com/example/adventure/controller/RequirementsController.java
@@ -1,5 +1,16 @@
 package com.example.adventure.controller;
 
+
+import com.example.adventure.service.RequirementsJPA;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RequirementsController {
+
+    private final RequirementsJPA reqJPA;
+
+    public RequirementsController(RequirementsJPA reqJPA) {
+        this.reqJPA = reqJPA;
+    }
 
 }
