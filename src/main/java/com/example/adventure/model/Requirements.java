0696322fd93f0ba6e1bcd7ca4cd9a97dffee6c29
ElisaMadsen <e.leisa@hotmail.com>
2022-09-26 11:48:12 +0200

    CRUD Activity done 1 iteration

--- src/main/java/com/example/adventure/model/Requirements.java
+++ src/main/java/com/example/adventure/model/Requirements.java
@@ -1,10 +1,14 @@
 package com.example.adventure.model;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Setter
@@ -48,6 +52,11 @@ public class Requirements {
 
     @Column
     public String requirementsDescrip;
+
+    @OneToMany(mappedBy = "requirements")
+    @JsonBackReference
+    @EqualsAndHashCode.Exclude
+    private Set<Activity> activities = new HashSet<>();
 
 
     public Requirements() {
