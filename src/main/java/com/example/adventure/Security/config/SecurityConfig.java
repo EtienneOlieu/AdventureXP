bd18f29db0bce2f65120e2f51fcf9ceeab634ac3
gnismas <csamsinges@gmail.com>
2022-10-11 09:49:03 +0200

    basic authorization added

--- src/main/java/com/example/adventure/Security/config/SecurityConfig.java
+++ src/main/java/com/example/adventure/Security/config/SecurityConfig.java
@@ -1,95 +1,103 @@
 package com.example.adventure.Security.config;
-import com.example.adventure.Security.jwt.AuthEntryPointJwt;
-import com.example.adventure.Security.jwt.AuthTokenFilter;
-import com.example.adventure.service.UserDetailsServiceImpl;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@AllArgsConstructor
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-@Autowired
-UserDetailsServiceImpl userDetailsService;
-    @Autowired
-    private AuthEntryPointJwt unauthorizedHandler;
-
-    @Bean
-    public AuthTokenFilter authenticationJwtTokenFilter() {
-        return new AuthTokenFilter();
-    }
-    @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/v1/**").permitAll()
-                .antMatchers("/api/v1/**").permitAll()
-                .anyRequest().authenticated();
-
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-    private final PasswordEncoder encoder;
-
+// basic authorization. Virker i backenden, men ikke med frontenden
     /*@Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http
-                .authorizeHttpRequests()
-                .antMatchers("/css/*", "/images/*", "/js/*")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/")
-               .permitAll()
-                //Husk at sætte vores landingpage på!
-                .defaultSuccessUrl("/navigation")
-                .failureUrl("/error");
 
-    }*/
-
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails adminUser = User.builder()
-                .username("admin")
-                .password(encoder.encode("admin"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(adminUser);
+    @Configuration
+    @EnableWebSecurity
+    public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    auth.inMemoryAuthentication()
+    .withUser("admin")
+    .password("admin")
+    .roles("admin_role");
     }
-}
+     */
+
+//    protected void configure(HttpSecurity http) throws Exception{
+//        httpBasic()
+//        .and()
+//        .authorizeRequests()
+//        .antMatchers("/api/v1/**).hasRole("admin_role")
+//        .and()
+//        .formLogin();
+//        }
+//      @Bean
+//      public PasswordEncoder getPasswordEncoder()
+//      {return NoOpPasswordEncoder.getInstance();}
+//
+//
+
+
+
+
+
+
+
+
+// et forsøg på at lave authorization med JWT
+//@AllArgsConstructor
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    UserDetailsServiceImpl userDetailsService;
+//    @Autowired
+//    private AuthEntryPointJwt unauthorizedHandler;
+//
+//    @Bean
+//    public AuthTokenFilter authenticationJwtTokenFilter() {
+//        return new AuthTokenFilter();
+//    }
+//
+//    @Override
+//    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder.inMemoryAuthentication()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .withUser("admin")
+//                .password("ADMIN")
+//                .roles("ADMIN")
+//        ;
+//    }
+//
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable()
+//                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//                .authorizeRequests().antMatchers("/api/v1/**").hasRole("ADMIN")
+//                .antMatchers("/api/v1/").permitAll()
+//                .anyRequest().authenticated();
+//
+//        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//    }
+//
+//
+//    private final PasswordEncoder encoder;
+//
+//
+//
+//
+//
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails adminUser = User.builder()
+//                .username("admin")
+//                .password(encoder.encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(adminUser);
+//    }
+
+
